21c4d5ba4afa7994534e3e9c73071b3ab4b4ddd0
Rowdy Rabouw <rowdy@double-r.nl>
2023-04-16 16:57:39 +0200

    feat: add before and after

---
+++ 09-properties/after/driver-quote.js
@@ -0,0 +1,43 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>@import "quote.css";</style>
+  <style>
+    :host {
+      --primary-color: #F74F33;
+      --secondary-color: #005b9c;
+    }
+    blockquote { 
+      color: var(--primary-color);
+    }
+    cite {
+      color: var(--secondary-color);
+    }
+  </style>
+  <blockquote><p></p><cite></cite></blockquote>
+`;
+
+class DriverQuote extends HTMLElement {
+  set quote(value) {
+    this._quote = value;
+    this.quoteElement.innerHTML = this._quote;
+  }
+
+  get quote() { return this._quote; }
+
+  set driver(value) {
+    this._driver = value;
+    this.driverElement.innerHTML = this._driver;
+  }
+
+  get driver() { return this._driver; }
+
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.quoteElement = this.shadowRoot.querySelector("p");
+    this.driverElement = this.shadowRoot.querySelector("cite");
+  }
+}
+
+customElements.define("driver-quote", DriverQuote);
